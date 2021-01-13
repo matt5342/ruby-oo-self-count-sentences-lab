e2f5ce1c717b5f8dfd8ecef83d24a174ebcda54d
Matt <mattfsewell@gmail.com>
2021-01-13 10:56:40 -0500

    commit tree 0d4ccfe1e68646418d24e02cda0e69d81f5c1a26

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[self.length - 1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    word_array = []
+    word_array = self.split(/[.!?]/)
+    # binding.pry
+    word_array.delete("")
+    word_array.count
   end
 end
